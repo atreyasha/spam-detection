d9563d39040a15a6e4d993b436e357f3292fea04
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-01-09 17:13:12 +0100

    New and improved pre-commit hook

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 # move pre-commit hook into local .git folder for activation
-cp ./hooks/pre-commit.sample ./.git/hooks/pre-commit
+cp ./hooks/pre-commit ./.git/hooks/pre-commit
