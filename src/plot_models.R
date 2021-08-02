2b7494d485efaae0f3acc84b51019df38c0bff37
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-08-02 14:13:43 +0200

    Lint R script

--- src/plot_models.R
+++ src/plot_models.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 # import dependencies
 library(RcppCNPy)
@@ -9,165 +9,195 @@ library(latex2exp)
 library(extrafont)
 library(ggsci)
 library(optparse)
-font_install('fontcm')
+font_install("fontcm")
 loadfonts()
 
 ###########################
 # functions for plots
 ###########################
 
-combinedPlotROC <- function(){
+combinedPlotROC <- function() {
   # make mapping for nice names
-  mapping <- c("CNN-LSTM (Words)"="rnn_words_random_embed","CNN-LSTM (Words+Characters)"="rnn_all_random_embed",
-               "CNN-LSTM (Words+GloVe)"="rnn_words_glove_embed","CNN-LSTM (Words+Characters+GloVe)"="rnn_all_glove_embed",
-               "SVM (Linear Kernel)"="svm_linear","SVM (RBF Kernel Approximation)"="svm_rbf")
+  mapping <- c(
+    "CNN-LSTM (Words)" = "rnn_words_random_embed", "CNN-LSTM (Words+Characters)" = "rnn_all_random_embed",
+    "CNN-LSTM (Words+GloVe)" = "rnn_words_glove_embed", "CNN-LSTM (Words+Characters+GloVe)" = "rnn_all_glove_embed",
+    "SVM (Linear Kernel)" = "svm_linear", "SVM (RBF Kernel Approximation)" = "svm_rbf"
+  )
   # read in all files and merge into single dataframe
-  for(str in c("test","blind")){
-    files <- list.files("./pickles",pattern=paste0("roc_",str,".npy"),recursive=TRUE,full.names=TRUE)
+  for (str in c("test", "blind")) {
+    files <- list.files("./pickles", pattern = paste0("roc_", str, ".npy"), recursive = TRUE, full.names = TRUE)
     store <- lapply(files, function(x) {
       store <- transpose(as.data.frame(npyLoad(x)))
-      names(store) <- c("fpr","tpr","threshold")
-      store["name"] <- names(mapping)[which(mapping==gsub("\\/.*","",gsub("(.*)(rnn|svm.*)","\\2",x)))]
-      store <- store[c(4,1:3)]
+      names(store) <- c("fpr", "tpr", "threshold")
+      store["name"] <- names(mapping)[which(mapping == gsub("\\/.*", "", gsub("(.*)(rnn|svm.*)", "\\2", x)))]
+      store <- store[c(4, 1:3)]
       # store <- store[seq(1, nrow(store), 100),]
       return(store)
     })
-    store <- do.call("rbind",store)
-    store$name <- factor(store$name,levels = names(mapping)[c(5,6,1,2,3,4)])
+    store <- do.call("rbind", store)
+    store$name <- factor(store$name, levels = names(mapping)[c(5, 6, 1, 2, 3, 4)])
     # prepare ggplot for combined object
-    pdf(paste0("../img/roc_",str,".pdf"), width=12, height=7)
-    g <- ggplot(data=store) + 
-      geom_step(aes(x=fpr,y=tpr,color=name),size=0.7) +
+    pdf(paste0("../img/roc_", str, ".pdf"), width = 12, height = 7)
+    g <- ggplot(data = store) +
+      geom_step(aes(x = fpr, y = tpr, color = name), size = 0.7) +
       geom_abline(intercept = 0, slope = 1, color = "black", linetype = "dashed") +
       theme_bw() +
-      xlab("\nFalse Positive Rate") + ylab("True Positive Rate\n") +
-      theme(text = element_text(size=13, family="CM Roman"),
-            legend.text=element_text(size=10),
-            legend.title=element_text(size=10,face="bold"),
-            legend.key = element_rect(colour = "lightgray", fill = "white"),
-            strip.text = element_text(size = 10,face="bold"),
-            plot.title= element_text(size = 14, hjust = 0.5, face="bold"),
-            legend.position = c(0.84,0.2),
-            legend.background = element_rect(fill="transparent")) +
+      xlab("\nFalse Positive Rate") +
+      ylab("True Positive Rate\n") +
+      theme(
+        text = element_text(size = 13, family = "CM Roman"),
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 10, face = "bold"),
+        legend.key = element_rect(colour = "lightgray", fill = "white"),
+        strip.text = element_text(size = 10, face = "bold"),
+        plot.title = element_text(size = 14, hjust = 0.5, face = "bold"),
+        legend.position = c(0.84, 0.2),
+        legend.background = element_rect(fill = "transparent")
+      ) +
       scale_color_ucscgb(name = "Model") +
-      ggtitle(paste0("ROC ",tools::toTitleCase(str)," Dataset"))
+      ggtitle(paste0("ROC ", tools::toTitleCase(str), " Dataset"))
     # print object
     print(g)
     dev.off()
-    embed_fonts(paste0("../img/roc_",str,".pdf"), outfile=paste0("../img/roc_",str,".pdf"))
+    embed_fonts(paste0("../img/roc_", str, ".pdf"), outfile = paste0("../img/roc_", str, ".pdf"))
   }
 }
 
-combinedPlot <- function(){
+combinedPlot <- function() {
   # make mapping for nice names
-  mapping <- c("CNN-LSTM (Words)"="rnn_words_random_embed","CNN-LSTM (Words+Characters)"="rnn_all_random_embed",
-               "CNN-LSTM (Words+GloVe)"="rnn_words_glove_embed","CNN-LSTM (Words+Characters+GloVe)"="rnn_all_glove_embed",
-               "SVM (Linear Kernel)"="svm_linear","SVM (RBF Kernel Approximation)"="svm_rbf")
+  mapping <- c(
+    "CNN-LSTM (Words)" = "rnn_words_random_embed", "CNN-LSTM (Words+Characters)" = "rnn_all_random_embed",
+    "CNN-LSTM (Words+GloVe)" = "rnn_words_glove_embed", "CNN-LSTM (Words+Characters+GloVe)" = "rnn_all_glove_embed",
+    "SVM (Linear Kernel)" = "svm_linear", "SVM (RBF Kernel Approximation)" = "svm_rbf"
+  )
   # read in all files and merge into single dataframe
-  files <- list.files("./pickles",pattern="precision_recall.*",recursive=TRUE,full.names=TRUE)
+  files <- list.files("./pickles", pattern = "precision_recall.*", recursive = TRUE, full.names = TRUE)
   store <- lapply(files, function(x) {
-    store <- read.csv(x,stringsAsFactors=FALSE)
-    store["name"] <- names(mapping)[which(mapping==gsub("\\/.*","",gsub("(.*)(rnn|svm.*)","\\2",x)))]
-    store <- store[c(4,1:3)]
-    store <- store[which(store["recall"]!=1),]
-    store <- store[which(store["recall"]!=0),]
+    store <- read.csv(x, stringsAsFactors = FALSE)
+    store["name"] <- names(mapping)[which(mapping == gsub("\\/.*", "", gsub("(.*)(rnn|svm.*)", "\\2", x)))]
+    store <- store[c(4, 1:3)]
+    store <- store[which(store["recall"] != 1), ]
+    store <- store[which(store["recall"] != 0), ]
     return(store)
   })
   optimal <- lapply(files, function(x) {
-    store <- read.csv(x,stringsAsFactors=FALSE)
-    store["name"] <- names(mapping)[which(mapping==gsub("\\/.*","",gsub("(.*)(rnn|svm.*)","\\2",x)))]
-    store <- store[c(4,1:3)]
-    store <- store[which(store["recall"]!=1),]
-    store <- store[which(store["recall"]!=0),]
-    if(sum(which(store["recall"]>=0.998)) == 0){
-      store <- store[which(store["recall"]==max(store["recall"])),]
+    store <- read.csv(x, stringsAsFactors = FALSE)
+    store["name"] <- names(mapping)[which(mapping == gsub("\\/.*", "", gsub("(.*)(rnn|svm.*)", "\\2", x)))]
+    store <- store[c(4, 1:3)]
+    store <- store[which(store["recall"] != 1), ]
+    store <- store[which(store["recall"] != 0), ]
+    if (sum(which(store["recall"] >= 0.998)) == 0) {
+      store <- store[which(store["recall"] == max(store["recall"])), ]
     } else {
-      store <- store[which(store["recall"]>=0.998),]
-      store <- store[which(store["precision"]==max(store["precision"])),]
+      store <- store[which(store["recall"] >= 0.998), ]
+      store <- store[which(store["precision"] == max(store["precision"])), ]
     }
     store$optimal <- "optimal"
     return(store)
   })
-  store <- do.call("rbind",store)
-  optimal <- do.call("rbind",optimal)
-  store$name <- factor(store$name,levels = names(mapping)[c(1,3,5,2,4,6)])
-  optimal$name <- factor(optimal$name,levels = names(mapping)[c(1,3,5,2,4,6)])
+  store <- do.call("rbind", store)
+  optimal <- do.call("rbind", optimal)
+  store$name <- factor(store$name, levels = names(mapping)[c(1, 3, 5, 2, 4, 6)])
+  optimal$name <- factor(optimal$name, levels = names(mapping)[c(1, 3, 5, 2, 4, 6)])
   # prepare ggplot for combined object
-  pdf("../img/combined.pdf", width=12, height=7)
-  g <- ggplot(data=store) + 
-    geom_point(aes(x=recall, y=precision, colour=threshold),size=1.2) +
-    geom_point(data = optimal, aes(x=recall, y=precision, fill=optimal),size=2, colour="red", alpha = 0.8) +
-    geom_line(aes(x=recall, y=precision, colour=threshold),size=0.5) +
-    geom_vline(xintercept = 0.998, linetype = "dashed", size = 0.25, colour = "red",alpha=0.6) +
+  pdf("../img/combined.pdf", width = 12, height = 7)
+  g <- ggplot(data = store) +
+    geom_point(aes(x = recall, y = precision, colour = threshold), size = 1.2) +
+    geom_point(data = optimal, aes(x = recall, y = precision, fill = optimal), size = 2, colour = "red", alpha = 0.8) +
+    geom_line(aes(x = recall, y = precision, colour = threshold), size = 0.5) +
+    geom_vline(xintercept = 0.998, linetype = "dashed", size = 0.25, colour = "red", alpha = 0.6) +
     theme_bw() +
-    ylim(c(min(store["precision"]),1.00)) + xlim(c(min(store["recall"]),1.00)) +
-    xlab("\nRecall") + ylab("Precision\n") +
-    theme(text = element_text(size=13, family="CM Roman"),
-          legend.text=element_text(size=10),
-          legend.title=element_text(size=10),
-          legend.key = element_rect(colour = "lightgray", fill = "white"),
-          strip.text = element_text(size = 10,face="bold")) +
+    ylim(c(min(store["precision"]), 1.00)) +
+    xlim(c(min(store["recall"]), 1.00)) +
+    xlab("\nRecall") +
+    ylab("Precision\n") +
+    theme(
+      text = element_text(size = 13, family = "CM Roman"),
+      legend.text = element_text(size = 10),
+      legend.title = element_text(size = 10),
+      legend.key = element_rect(colour = "lightgray", fill = "white"),
+      strip.text = element_text(size = 10, face = "bold")
+    ) +
     scale_colour_continuous(type = "viridis", guide = "colourbar") +
     # scale_colour_gradientn(colours = terrain.colors(20)) +
     # scale_colour_viridis_c() +
-    scale_fill_discrete(name="",
-                        breaks=c("optimal"),
-                        labels=c("Optimal\nThreshold")) +
-    facet_wrap(~name, ncol=3) +
-    guides(colour = guide_colourbar(title="Threshold", barwidth = 0.9, barheight = 18))
+    scale_fill_discrete(
+      name = "",
+      breaks = c("optimal"),
+      labels = c("Optimal\nThreshold")
+    ) +
+    facet_wrap(~name, ncol = 3) +
+    guides(colour = guide_colourbar(title = "Threshold", barwidth = 0.9, barheight = 18))
   # print object
   print(g)
   dev.off()
-  embed_fonts("../img/combined.pdf", outfile="../img/combined.pdf")
+  embed_fonts("../img/combined.pdf", outfile = "../img/combined.pdf")
 }
 
-svmWordPlot <- function(){
-  files <- list.files("./pickles",recursive=TRUE,full.names=TRUE)
-  files <- grep("ham|spam",grep("linear",files,value=TRUE),value=TRUE)
+svmWordPlot <- function() {
+  files <- list.files("./pickles", recursive = TRUE, full.names = TRUE)
+  files <- grep("ham|spam", grep("linear", files, value = TRUE), value = TRUE)
   dir <- dirname(files[1])
-  ham_words <- read.csv(grep("spam.*csv$",files,value=TRUE),stringsAsFactors=FALSE)
-  spam_words <- read.csv(grep("ham.*csv$",files,value=TRUE),stringsAsFactors=FALSE)
+  ham_words <- read.csv(grep("spam.*csv$", files, value = TRUE), stringsAsFactors = FALSE)
+  spam_words <- read.csv(grep("ham.*csv$", files, value = TRUE), stringsAsFactors = FALSE)
   ham_words$name <- "bottom"
   spam_words$name <- "top"
   # make plot for top words
-  pdf(paste0(dir,"/spam_words.pdf"), width=12, height=8)
-  g <- ggplot(data=spam_words, aes(reorder(word,coefficient,sum),coefficient)) + geom_col(fill = "red",colour="black",
-                                                                                         size=0.34,alpha = 0.8) +
-    theme_bw() + theme(text=element_text(size=16,family="CM Roman"),legend.position="none", plot.title=element_text(hjust = 0.5),
-                       axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
-                       axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0))) + 
-    xlab("Word feature") + ylab(TeX("Absolute weight |$\\theta$|")) +
+  pdf(paste0(dir, "/spam_words.pdf"), width = 12, height = 8)
+  g <- ggplot(data = spam_words, aes(reorder(word, coefficient, sum), coefficient)) +
+    geom_col(
+      fill = "red", colour = "black",
+      size = 0.34, alpha = 0.8
+    ) +
+    theme_bw() +
+    theme(
+      text = element_text(size = 16, family = "CM Roman"), legend.position = "none", plot.title = element_text(hjust = 0.5),
+      axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
+      axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0))
+    ) +
+    xlab("Word feature") +
+    ylab(TeX("Absolute weight |$\\theta$|")) +
     ggtitle(TeX("Ranked spam |$\\theta$| coefficients"))
   print(g)
   dev.off()
-  embed_fonts(paste0(dir,"/spam_words.pdf"), outfile=paste0(dir,"/spam_words.pdf"))
+  embed_fonts(paste0(dir, "/spam_words.pdf"), outfile = paste0(dir, "/spam_words.pdf"))
   # make plot for bottom words
-  pdf(paste0(dir,"/ham_words.pdf"), width=12, height=8)
-  g <- ggplot(data=ham_words, aes(reorder(word,coefficient,sum),coefficient)) + geom_col(fill = "blue",colour="black",
-                                                                                         size=0.34,alpha = 0.8) +
-    theme_bw() + theme(text=element_text(size=16,family="CM Roman"),legend.position="none", plot.title=element_text(hjust = 0.5),
-                       axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
-                       axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0))) + 
-    xlab("Word feature") + ylab(TeX("Absolute weight |$\\theta$|")) +
+  pdf(paste0(dir, "/ham_words.pdf"), width = 12, height = 8)
+  g <- ggplot(data = ham_words, aes(reorder(word, coefficient, sum), coefficient)) +
+    geom_col(
+      fill = "blue", colour = "black",
+      size = 0.34, alpha = 0.8
+    ) +
+    theme_bw() +
+    theme(
+      text = element_text(size = 16, family = "CM Roman"), legend.position = "none", plot.title = element_text(hjust = 0.5),
+      axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
+      axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0))
+    ) +
+    xlab("Word feature") +
+    ylab(TeX("Absolute weight |$\\theta$|")) +
     ggtitle(TeX("Ranked ham |$\\theta$| coefficients"))
   print(g)
   dev.off()
-  embed_fonts(paste0(dir,"/ham_words.pdf"), outfile=paste0(dir,"/ham_words.pdf"))
+  embed_fonts(paste0(dir, "/ham_words.pdf"), outfile = paste0(dir, "/ham_words.pdf"))
 }
 
 ###########################
 # main command call
 ###########################
 
-option_list = list(
-  make_option(c("-t", "--type"), type="character", default="combined", 
-              help="which process to execute, either 'combined' or 'svm'", metavar="character"))
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
-if(opt$type == "combined"){
+option_list <- list(
+  make_option(c("-t", "--type"),
+    type = "character", default = "combined",
+    help = "which process to execute, either 'combined' or 'svm'", metavar = "character"
+  )
+)
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
+if (opt$type == "combined") {
   combinedPlot()
-} else if(opt$type == "svm"){
+} else if (opt$type == "svm") {
   svmWordPlot()
-} else if(opt$type == "combined_roc"){
+} else if (opt$type == "combined_roc") {
   combinedPlotROC()
 }
